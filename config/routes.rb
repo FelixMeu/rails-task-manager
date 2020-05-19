3b387df4a9c9fc78213451814fce36e71fab9ce9
Felix Meumann <f.meumann@gmail.com>
2020-05-19 12:50:22 +0200

    3 - As a user, I can list tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'tasks', to: 'tasks#index', as: :tasks
+  # get "tasks/:id", to: "tasks#show", as: :tasks
 end
 
 # add new route to list tasks (conventions lecture)
